00f80fa30917d76c63dede8e39efbba30755d620
Sitch <michael@learnvest.com>
2013-07-27 16:55:06 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-Student-Lending-Project-Webservice
-==================================
+# aiahoao
+
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2013 FIXME
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject clojure-rest "0.1.0-SNAPSHOT"
+      :description "REST Serviec"
+      :url "http://www.studentlendingproject.com"
+      :dependencies [[org.clojure/clojure "1.5.1"]
+                     [compojure "1.1.5"]
+                     [ring/ring-json "0.2.0"]
+                     [c3p0/c3p0 "0.9.1.2"]
+                     [org.clojure/java.jdbc "0.3.0-alpha4"]
+                     [com.h2database/h2 "1.3.168"]
+                     [cheshire "5.2.0"]]
+      :plugins [[lein-ring "0.8.6"]]
+      :ring {:handler clojure-rest.handler/app}
+      :profiles
+      {:dev {:dependencies [[ring-mock "0.1.5"]]}})

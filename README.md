1485441b61064f71a9f0a9d04f4abb202c4aaf65
Michael Sitchenko <michael.sitchenko@gmail.com>
2013-08-10 18:32:50 -0400

    update readme.md and project file

--- README.md
+++ README.md
@@ -6,3 +6,76 @@
 lein uberjar
 java -jar target/studentlendingproject-0.1.0-SNAPSHOT-standalone.jar server
 ```
+
+
+## Deploy
+```
+<!-- export RING_ENV=production -->
+lein ring uberwar
+```
+
+
+
+# AWS SETUP
+
+##Credentials
+```
+:access-key 
+:secret-key
+
+```
+
+
+```
+<!-- Java Home Path -->
+export JAVA_HOME=/path/to/jdk
+
+<!-- Java Executables Path -->
+export PATH=$JAVA_HOME/bin:$PATH
+
+<!-- ************* -->
+<!-- DATOMIC SETUP -->
+<!-- ************* -->
+bin/transactor conf/free-aws-transactor.properties
+
+java -jar target/studentlendingproject-0.0.1-standalone.jar db/reload
+
+
+
+```
+
+# PALLET
+
+From: http://stackoverflow.com/questions/17028427/how-to-deploy-a-clojure-web-application-to-amazon-ec2-aws-elastic-beanstalk-l
+
+##Group spec
+```
+(def java-server 
+  (java/server-spec {
+    :vendor :oracle 
+    :components #{:jdk} 
+    :version [7]}))
+```
+
+##Node spec
+```
+(def webserver
+ (node-spec
+   :image {:os-family :ubuntu} 
+   :hardware {:min-cores 1 :min-ram (* 2 1024)}
+   :phases {:configure
+            (plan-fn
+               (package-manager :update)
+               (package "tomcat7")
+            (remote-file "/var/lib/tomcat7/webapps/myapp.war"
+               :local-file "target/myApp.war"
+               :owner "tomcat7"
+               :group "tomcat7"
+               :mode 755))
+
+(def web-group
+ (group-spec
+   "my-websertvers"
+   webserver
+   :extends [java-server]))
+```
--- project.clj
+++ project.clj
@@ -1,36 +1,49 @@
-(defproject studentlendingproject "0.1.0-SNAPSHOT"
+(defproject slp "0.0.1-SNAPSHOT"
   :description "Student Lending Project"
   :url "http://www.studentlendingproject.com"
   :repositories [["central-proxy" "http://repository.sonatype.org/content/repositories/central/"]]
-  :resource-paths ["lib/*"]
-  :jvm-opts ["-Xmx2G"]
 
+;  :omit-source true
+  :jvm-opts ["-Xmx2G"]
+  :resource-paths ["resources"]
+  :uberjar-name "slp-standalone.jar"
+  
+  :pedantic? :abort ;abort on dependency version inconsistencies
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/tools.namespace "0.2.4"]
+                 [org.clojure/data.json "0.2.2"]
+                 
+                 ;; Database
                  [com.datomic/datomic-free "0.8.4122"]
-                 [environ "0.4.0"]
+                 
+                 ;; Web Services
                  [ring "1.2.0"]
                  [ring-mock "0.1.5"]
                  [ring-middleware-format "0.3.0"]
                  [compojure "1.1.5"]
                  [liberator "0.9.0"]
-                 [com.cemerick/friend "0.1.5"]
-                 [crypto-random "1.1.0"]
-                 [org.clojure/tools.namespace "0.2.4"]
-                 [com.flyingmachine/webutils "0.1.1"]
-                 [flyingmachine/cartographer "0.1.1"]
-                 [markdown-clj "0.9.29"]
-                 [clavatar "0.2.1"]
-                 [org.apache.commons/commons-email "1.3.1"]
-                 [org.clojure/data.json "0.2.2"]
-                 [org.webbitserver/webbit "0.4.15"]]
+                 [environ "0.4.0"] ;environment
+                 [crypto-random "1.1.0"] ;crypto
+                 [com.cemerick/friend "0.1.5"] ;auth
+                 [org.webbitserver/webbit "0.4.15"] ;web sockets
+                 
+                 [prismatic/plumbing "0.1.0"] ;prismatic
+                 
+                 [com.flyingmachine/webutils "0.1.1"] ;paths
+                 [flyingmachine/cartographer "0.1.1"] ;mapping
+                 
+                 ;; Deployment using Palete
+                 [org.clojars.strad/datomic-crate "0.8.8"]]
 
-  :plugins [[lein-environ "0.4.0"]
-            [lein-beanstalk "0.2.7"]
-            [lein-ancient "0.4.4"]]
+  :plugins [[lein-ancient "0.4.4"]
+            [lein-environ "0.4.0"]
+            [com.palletops/pallet-lein "0.8.0-alpha.1"]]
 
-  :resource-paths ["resources"]
-  
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
-
+  
+  ;; Pallet
+;  :pallet {:source-paths ["src-pallet"] :resource-paths []}
+;  :pallet {:dependencies [[org.virtualbox/vboxjws "4.2.6"]]}
+  
   :main slp.app)
